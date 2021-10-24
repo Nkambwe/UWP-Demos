0eb2f0a29e09041a9ffacb6ec7631dc2019d2894
Nkambwe <jo.jomac.mac801@gmail.com>
2021-10-24 17:47:50 +0300

    Initial Commit

---
+++ UwpExampleApp/UwpExampleApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UwpExampleApp {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
